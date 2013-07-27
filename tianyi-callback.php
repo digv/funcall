da84c9583e9ae4fbe0775b342d2cf851bd2cc324
gefferey <250276522@qq.com>
2013-07-27 11:13:28 +0800

    dd

--- tianyi-callback.php
+++ tianyi-callback.php
@@ -1,5 +1,21 @@
 <?php
+session_start();
+
 include_once 'inc/config.php';
 include_once 'inc/TYauth.php';
 
 $o = new TYAuthV2(APP_ID, APP_SECRET);
+
+if ($_REQUEST['code']) {
+	
+	$keys = array();
+	$keys['code'] = $_REQUEST['code'];
+	$keys['redirect_uri'] = TY_CALL_BACK;
+	try {
+		$token = $o->getAccessToken( 'code', $keys ) ;
+		var_dump($token);
+	} catch (Exception $e) {
+		
+	}
+}
+
